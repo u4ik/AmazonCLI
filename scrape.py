969dcea6164815973b6b8c2cf0e1d9aad6d3d23e
u4ik <insighteuphoric@gmail.com>
2021-11-10 21:35:42 -0500

    initial

--- scrape.py
+++ scrape.py
@@ -1,25 +1,28 @@
 from bs4 import BeautifulSoup
 
-''' Opening the test.html file. "r" is passed as we want to read this file. 
-# The result is stored in 'file'.'''
+# ? Opening the test.html file. "r" is passed as we want to read this file. 
+# ? The result is stored in 'file'.
 with open('test.html', "r") as file:
-    '''Using BeautifulSoup to read the result, and parse it using a build in parser, and storing the results in 'doc'''
+# ? Using BeautifulSoup to read the result, and parse it using a build in parser, and storing the results in 'doc'''
     doc = BeautifulSoup(file, "html.parser")
 
-'''Can use the doc class with dot notation to access any tag from the html document.'''
+# ? Can use the doc class with dot notation to access any tag from the html document.
 tag = doc.title
 
-'''To access the string that's contained within the tag. Which in this example would result in 'Document', the text between the title tag.'''
+# ?To access the string that's contained within the tag. Which in this example would result in 'Document', the text between the title tag.'''
+
 # print(tag.string)
 
-'''You can modify the string within tags as well:'''
+# ? You can modify the string value within tags as well:
 # tag.string = 'something else'
 # print(tag)
 
-'''Using another method from doc class, you can also find tags, along with indexing to select a specifc one'''
+# ? Using another method from doc class, you can also find tags, along with indexing to select a specific one. Looking for the first div tag
+
 foundTags = doc.find_all('div')[0]
 
-'''Then you can find any nested tag, as main is contained within the fist div'''
+# ? Then you can find ANY nested tags, in this case, main is contained within the fist div
+
 print(foundTags('main'))
 
 # print(doc.prettify())
