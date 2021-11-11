ecef94a9f2ab1d06f3125d271966d0f4eb16bd7b
u4ik <insighteuphoric@gmail.com>
2021-11-11 17:40:29 -0500

    results stored in a dict, however need to omit the additional prices that come up

--- scrape.py
+++ scrape.py
@@ -3,15 +3,22 @@ from os import write
 from pprint import pprint
 import requests
 
+
 def start():
-    q = input("Search Amazon For: ")
-    # q = 'ps5'
+    # q = input("Search Amazon For: ")
+    q = 'ps5'
+
     s_url = f'https://www.amazon.com/s?k={q}'
     resp = request(s_url)
-    # write(str(parse(res)))
     res = parse(resp)
-    pprint(f'Results: {len(res)}')
-    pprint(res)
+
+    # ? PRINT FINAL RESULTS:
+    # pprint(f'Results: {len(res)}')
+    # pprint(res)
+
+    # ? SAVE FINAL RESULTS:
+    # write(str(res))
+
 
 def request(url):
     req_headers = {
@@ -21,40 +28,47 @@ def request(url):
     response = requests.get(url, headers=req_headers).text
     return response
 
+
 def parse(res):
     c_d = {}
     b = BeautifulSoup(res, "html.parser")
     # old
-    p_wrap = b.find_all(
-        'a', class_="a-size-base a-link-normal a-text-normal")
+    # p_wrap = b.find_all('a', class_="a-size-base a-link-normal a-text-normal")
 
 # testing
-    # p_wrap = b.findChildren('div', class_='s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16')
+    p_wrap = b.findChildren('div', class_='s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16')
+
+    pprint(p_wrap)
 
     # print(p_wrap.encode("utf-8"))
     write(str(p_wrap))
     p = []
-    for i in range(len(p_wrap)):
-        tmp1 = p_wrap[i].findChildren(
-            "span", class_="a-price", attrs={"data-a-size": "l", "data-a-color": "base"}, recursive=True)
-        for k in range(len(tmp1)):
-            tmp2 = tmp1[k].findChildren(
-                "span", class_="a-offscreen", recursive=True)
-            for j in range(len(tmp2)):
-                p.append(tmp2[j])
-    descs = b.find_all(
-        "span", class_="a-size-medium a-color-base a-text-normal")
-    print(f'Prices: Amount {len(p)}')
-    print(f'Desc: Amount {len(descs)}')
-    for i in range(len(descs)):
-        c_d[i] = {'Desc':descs[i].string}
-    for i in range(len(p)):
-        # print(i,p[i])
-        if i <= len(c_d) - 1:
-            c_d[i]['Price'] = p[i].text 
-    return c_d
+
+
+    # for i in range(len(p_wrap)):
+    #     tmp1 = p_wrap[i].findChildren(
+    #         "span", class_="a-price", attrs={"data-a-size": "l", "data-a-color": "base"}, recursive=True)
+    #     for k in range(len(tmp1)):
+    #         tmp2 = tmp1[k].findChildren(
+    #             "span", class_="a-offscreen", recursive=True)
+    #         for j in range(len(tmp2)):
+    #             p.append(tmp2[j])
+    # descs = b.find_all(
+    #     "span", class_="a-size-medium a-color-base a-text-normal")
+    # print(f'Prices: Amount {len(p)}')
+    # print(f'Desc: Amount {len(descs)}')
+    # for i in range(len(descs)):
+    #     c_d[i] = {'Desc': descs[i].string}
+    # for i in range(len(p)):
+    #     # print(i,p[i])
+    #     if i <= len(c_d) - 1:
+    #         c_d[i]['Price'] = p[i].text
+    # return c_d
+
 
 def write(f):
     with open('results.html', "w") as fl:
         fl.write(f)
+
+
 start()
