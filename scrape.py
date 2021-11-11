951a1b8afbf03347fe80627e199fb3befda5510c
u4ik <insighteuphoric@gmail.com>
2021-11-10 21:11:29 -0500

    initial

---
+++ scrape.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+# Opening the test.html file. "r" is passed as we want to read this file. 
+# The result is stored in 'file'.
+with open('test.html', "r") as file:
+    # Using BeautifulSoup to read the result, and parse it using a build in parser, and storing the results in 'doc'
+    doc = BeautifulSoup(file, "html.parser")
+
+
+'''
+Can use the doc class with dot notation to access any tag from the html document.
+'''
+tag = doc.title
+
+print(tag) 
+
+# print(doc.prettify())
