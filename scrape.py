a9ef232bccf96ea7050249f06a5de0526f690453
u4ik <insighteuphoric@gmail.com>
2021-11-11 04:01:22 -0500

    removed the strikeout price, however alternative prices still remain, ex. games for different platforms.

--- scrape.py
+++ scrape.py
@@ -16,6 +16,7 @@ with open('test.html', "r") as file:
     # ? Using BeautifulSoup to read the result, and parse it using a build in parser, and storing the results in 'doc'''
     doc = BeautifulSoup(file, "html.parser")
 
+
 # ? Can use the doc class with dot notation to access any tag from the html document.
 tag = doc.title
 
@@ -56,22 +57,14 @@ def make_requests(url):
         "Content-Type": "text/plain; charset=utf-8",
         "User-Agent": r"Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) WindowsPowerShell/5.1.19041.1023"
     }
-    req = requests.get(url, headers=req_headers)
-    return req
+    response = requests.get(url, headers=req_headers)
+    return response
 
-def make_request(url):
-    req_headers = {
-        "Content-Type": "text/plain; charset=utf-8",
-        "User-Agent": r"Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) WindowsPowerShell/5.1.19041.1023"
-    }
-    req = request.Request(str(url))
-    for header in req_headers:
-        req.add_header(header, req_headers[header])
-    body = request.urlopen(req, None, 300).read().decode("utf-8")
-    return body
 
 # ? Using request module from urllib
 # body = BeautifulSoup(make_requests(url).text, "html.parser")
+
+
 
 # ? Using requests module
 body = BeautifulSoup(make_requests(multipleItemsUrl).text, "html.parser")
@@ -85,6 +78,12 @@ priceWrap = body.find_all('a',class_="a-size-base a-link-normal a-text-normal")
 
 
 
+
+
+
+
+
+
 prices= []
 for i in range(len(priceWrap)):
     tmp1 =  priceWrap[i].findChildren("span", class_="a-price", attrs={"data-a-size" : "l", "data-a-color": "base"},recursive=True)
@@ -93,21 +92,13 @@ for i in range(len(priceWrap)):
         for i in range(len(tmp2)):
 
             prices.append(tmp2[i])
-    # print(tmp2)
 
 
-# for i in range(len(tmp1)):
-    # print(tmp1[0].findChildren(""))
-    # tmp2.append(tmp1[i].find("span", class_="a-price", recursive=True))
-
-    # res = tmp[i].findChildren("span", class_="a-whole-price", recursive=True)
-    # for i in res:
-    #     prices.append(i.text)
-
-    # p = res.text
 
 
-# parent = prices[0].parent
+
+
+
 descs = body.find_all("span", class_="a-size-medium a-color-base a-text-normal")
 
 holder = {}
@@ -115,9 +106,9 @@ holder = {}
 print(f'Prices: Amount {len(prices)}')
 print(f'Desc: Amount {len(descs)}')
 
-# print(prices)
 
-# TODO Prices solved, now need to store the results
+
+
 for i in range(len(descs)):
     holder[i] = {descs[i].string}
   
@@ -127,9 +118,12 @@ for i in range(len(prices)):
         print(holder[i])
         print(prices[i].text)
 
-# holder[i] = {"price": prices[i].string,**holder[i]}
-# holder[i] = {"price": prices[i].string}
+
 
 # pprint(holder)
 
+
+def writeFile():
+    with open('ps5-amazon.html', "w") as file:
+        file.write(priceWrap)
 
