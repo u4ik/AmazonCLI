1037b1e065fe4b99ccec94ff676066fa608e5f4d
u4ik <insighteuphoric@gmail.com>
2021-11-10 21:14:00 -0500

    initial

--- scrape.py
+++ scrape.py
@@ -6,12 +6,15 @@ with open('test.html', "r") as file:
     # Using BeautifulSoup to read the result, and parse it using a build in parser, and storing the results in 'doc'
     doc = BeautifulSoup(file, "html.parser")
 
-
 # Can use the doc class with dot notation to access any tag from the html document.'''
-tag = doc.div
+tag = doc.title
 
-# To access the string that's contained within the tag, 
+# To access the string that's contained within the tag. Which in this example would result in 'Document', the text between the title tag.
+print(tag.string)
 
-print(tag)
+
+
+
+
 
 # print(doc.prettify())
