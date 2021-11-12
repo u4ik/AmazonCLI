e5b746d7c37c14fbe8f537b179de7209173ecf0c
u4ik <insighteuphoric@gmail.com>
2021-11-12 04:14:38 -0500

    working on sort

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 <h1>Scrape Amazon Products</h1>
-<p>Enter a search term.</p>
+<p>Enter a <b>search term</b>.</p>
 <img width=300em src='https://i.ibb.co/X3kTvVd/search-term.png'/>
-<p>Input the minimum price.</p>
+<p>Enter a <b>minimum</b> price.</p>
 <img width=300em src='https://i.ibb.co/bLZ9hRs/min-price.png'/>
-<p>Input the maximum price.</p>
+<p>Enter a <b>maximum</b> price.</p>
 <img width=300em src='https://i.ibb.co/Pty5t2P/max-price.png'/>
+<p>Optional sort</p>
+<img width=300em src='https://i.ibb.co/LvRpbmz/sortbyprice.png'/>
+<p>Sort Options</p>
+<img src='https://i.ibb.co/Yc2wWts/sortby.png'/>
+
+<p>Confirm</p>
+<img src='https://i.ibb.co/Kr3t5jL/confirm.png'/>
+
 <p>Results will be logged.</p>
-<img src='https://i.ibb.co/Tvy8KH8/results.png'/>
-<p>Results will also be stored in a "results.json" file.</p>
-<img src='https://i.ibb.co/kqY1DJJ/resultsjson.png'/>
+<img src='https://i.ibb.co/bLxxKy8/results.png'/>
+<p>Results will also be stored in a <b>results.json</b> file.</p>
+<img src='https://i.ibb.co/hR4Qdf1/resultsjson.png'/>
--- scrape.py
+++ scrape.py
@@ -7,84 +7,84 @@ from InquirerPy import prompt
 
 
 def start():
-    # q = input("Search Amazon For: ")
-    # m_p = input("Min Price: ")
-    # mx_p = input("Max Price: ")
+    try:
+        q = input("Search Amazon For: ")
+        
+        while( len(q) == 0):
+            print('Enter a search term!')
+            q = input("Search Amazon For: ")
+            if len(q) > 0:
+                break
 
-    sorted_c_d = {}
-    q = 'ps5'
-    m_p = 500
-    mx_p = 1000
+        m_p = input("Min Price: ")
 
-    s_url = f'https://www.amazon.com/s?k={q}&rh=p_36%3A{m_p}00-{mx_p}00'
-    resp = request(s_url)
-    questions = [
-        {"type": "confirm", "message": "Sort by price?", "name": "sort"},
-        {"type": "list", "message": "Sort by...", "name": "sortchoice", "choices": [
-            "Highest Price",
-            "Lowest Price"
-        ]},
-        {"type": "confirm",
-            "message": "Save output file? (results.json):", "name": "output"},
-        {"type": "confirm", "message": "Confirm?", "name": "confirm"},
-    ]
+        while( len(m_p) == 0):
+            print('Enter a minimum price!')
+            m_p = input("Min Price: ")
+            if len(m_p) > 0:
+                break
 
 
-    res = parse(resp)
+        mx_p = input("Max Price: ")
 
-    sorted_arr = sorted(res, key=res.get('Price_Val:'))
-    # sorted_arr = sorted(res.items(), key=lambda x: x[1]['Price_Val:'])
-    
 
-    for i in sorted_arr:
-        print(i)
-        # sorted_c_d[i] = k
+        while( len(mx_p) == 0):
+            print('Enter a maximum price!')
+            mx_p = input("Max Price: ")
+            if len(mx_p) > 0:
+                break
+        
 
-        # sorted_c_d[i[0]] = {i[1]['Price_Val:']}
-        # sorted_c_d[i[0]] = {"Desc:":i[1]['Desc:']}
-        # sorted_c_d[i[0]]['Price:'] = i[1]['Price_Val:']
 
-        # print(i[0])
-        # print(i[1]['Desc:'])
-        # print(i[1]['Price:'])
-        # print(i[1]['Price_Val:'])
+        sorted_c_d = []
+        # q = 'ps5'
+        # m_p = 500
+        # mx_p = 1200
+        
+        s_url = f'https://www.amazon.com/s?k={q}&rh=p_36%3A{m_p}00-{mx_p}00'
+        resp = request(s_url)
+        questions = [
+            {"type": "confirm", "message": "Sort by price?", "name": "sort"},
+            {"type": "list", "message": "Sort by...", "name": "sortchoice", "choices": [
+                "Highest Price",
+                "Lowest Price"
+            ]},
+            {"type": "confirm",
+                "message": "Save output file? (results.json):", "name": "output"},
+            {"type": "confirm", "message": "Confirm?", "name": "confirm"},
+        ]
 
-    pprint(sorted_c_d)
+        res = parse(resp)
 
-    # sort_items = sorted(res.items(), key=lambda x: (x[2]['Price_Val']))
-    # print(sort_items)
+        sorted_c_d = res
 
-    # sort_result = prompt(questions[0])
-    # sort = sort_result["sort"]
+        sort_result = prompt(questions[0])
+        sort = sort_result["sort"]
 
-    # if sort:
-    #     sort_result = prompt(questions[1])
-    #     sort_choice = sort_result["sortchoice"]
+        if sort:
+            sort_result = prompt(questions[1])
+            sort_choice = sort_result["sortchoice"]
 
-    # outresult = prompt(questions[2])
-    # output = outresult["output"]
+        outresult = prompt(questions[2])
+        output = outresult["output"]
 
-    # confirmresult = prompt(questions[-1])
-    # confirm = confirmresult["confirm"]
+        confirmresult = prompt(questions[-1])
+        confirm = confirmresult["confirm"]
 
-    # if confirm:
-    #     res = parse(resp)
-    #     sort_items = sorted(res.items(), key=lambda x: x[1]['Price'])
-    #     print(sort_items)
-    #     if sort:
-    #         if sort_choice[0] == "H":
-    #             sort_price('h', res )
-    #         else:
-    #             sort_price('l', res )
-    #     else:
-    #         pprint(f'{len(res)} Results')
-    #         # pprint(res)
-    #         if output:
-    #             write(str(res))
-    #         else:
-    #             return
-    # else:
-    #     return
+        if confirm:
+            if sort:
+                if sort_choice[0] == "H":
+                    sorted_c_d = sort_price('h', res)
+                else:
+                    sorted_c_d = sort_price('', res)
+            if output:
+                write(str(sorted_c_d))
+            pprint(sorted_c_d)
+            pprint(f'{len(sorted_c_d)} Results')
+        else:
+            return
+    except(KeyboardInterrupt) as e:
+        print('\nExiting...')
 
 
 def request(url):
@@ -97,19 +97,15 @@ def request(url):
 
 
 def sort_price(flag, res):
-
-    # sort_items = sorted(res.items())
-    # print()
-
     if flag == "h":
-        print('sorting highest')
+        return sorted(res, key=lambda x: x['Price_Val:'], reverse=True)
     else:
-        print('sorting lowest')
+        return sorted(res, key=lambda x: x['Price_Val:'])
 
 
 def parse(res):
     t = 'div'
-    c_d = {}
+    c_d = []
     c = 0
     b = BeautifulSoup(res, "html.parser")
     p_w = b.findChildren(
@@ -123,10 +119,14 @@ def parse(res):
             try:
                 x = r[2].findChildren('span', class_="a-offscreen")
                 for i in x[0]:
-                    c_d[c] = {'Desc:': l.h2.text}
-                    c_d[c]['Price:'] = i.string
-                    c_d[c]['Price_Val:'] = int(
-                        i.string.replace("$", "").replace(".", "")[:-2])
+                    o = {
+                        'id': c,
+                        'Desc': l.h2.text,
+                        'Price': i.string,
+                        'Price_Val:': int(
+                            i.string.replace("$", "").replace(".", "").replace(",","")[:-2]),
+                    }
+                    c_d.append(o)
                     c += 1
             except (NameError, AttributeError, IndexError) as e:
                 continue
