6c83b0cba44252bb92dc9ca5a6dd724a1563e625
u4ik <insighteuphoric@gmail.com>
2021-11-11 20:56:06 -0500

    results stored in a dict, however need to omit the additional prices that come up

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from os import write
 from pprint import pprint
 import requests
-
+import re
 
 def start():
     # q = input("Search Amazon For: ")
@@ -36,12 +36,44 @@ def parse(res):
     # p_wrap = b.find_all('a', class_="a-size-base a-link-normal a-text-normal")
 
 # testing
-    p_wrap = b.findChildren('div', class_='s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16')
+    p_wrap = b.findChildren('div', class_='s-main-slot s-result-list s-search-results sg-row')
 
-    pprint(p_wrap)
+
+    tmp = []
+    for p in p_wrap:
+        f_c = p.findChildren('div', class_='s-result-item', attrs={"data-index": True, "data-asin": True } ,recursive=True)
+        
+
+        for l in f_c[1:]:
+            r = l.findChildren("div", class_="sg-col-inner")
+            print(r.h2.text)
+            for f in r:
+                d = f.findChildren('h2')
+                # for i in d:
+                    # print(i.text)
+
+                   
+    
+
+
+         
+            # print(l)
+        # print(f_c)
+        # for c in f_c:
+            # item = c.findChildren('div', class_="s-asin")
+            # print(item)
+            # c2 = c.findChildred('div', attrs={'data-index':1} )
+            # pprint(len(c2))
+                # if c.has_attr('data-asin'):
+                #     print(len(c))
+                #     if c.has_attr('data-component-type'):
+                #         # pprint(c.prettify())
+                #         write(str(c))
+    write(str(tmp))
+    # pprint(tmp)
+            
 
     # print(p_wrap.encode("utf-8"))
-    write(str(p_wrap))
     p = []
 
 
@@ -67,7 +99,7 @@ def parse(res):
 
 
 def write(f):
-    with open('results.html', "w") as fl:
+    with open('results.html', "w",encoding='utf-8') as fl:
         fl.write(f)
 
 
